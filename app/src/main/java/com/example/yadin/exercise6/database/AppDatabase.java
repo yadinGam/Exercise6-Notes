9ff9f9352d6f814af50ea26bdf7fec4caffa4e34
yadinGam <yadingaml@gmail.com>
2018-04-18 19:18:42 +0300

    test

---
+++ app/src/main/java/com/example/yadin/exercise6/database/AppDatabase.java
@@ -0,0 +1,39 @@
+package com.example.yadin.exercise6.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.example.yadin.exercise6.Note;
+
+/**
+ * Created by yadin on 18/04/2018.
+ */
+
+@Database(entities = {Note.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase{
+    private static final String DATABASE_NAME = "notes";
+
+    public abstract NoteDao noteDao();
+
+    private static AppDatabase singleton;
+
+    public static AppDatabase getAppDatabase(Context context) {
+
+        if (singleton == null) {
+            synchronized (AppDatabase.class) {
+                if (singleton == null) {
+                    singleton = Room.databaseBuilder(context.getApplicationContext(),
+                            AppDatabase.class,
+                            DATABASE_NAME)
+                            .build();
+                }
+            }
+        }
+        return singleton;
+
+    }
+
+
+}
